cc895a317e925a983d8ecf6566b2676599192ff8
yildirim038 <aosmanyildirim@gmail.com>
2022-02-26 23:37:46 +0100

    Class eklendi

--- src/week1/assignment1/application.java
+++ src/week1/assignment1/application.java
@@ -20,6 +20,8 @@ public class application {
                 {"Mehmet","Hamdi","15/3/1986","Ingilizce"}
         };
 
+        String[][] group1= {Students[0],Students[1],Students[2],Students[3],Students[4]};
+        String[][] group2= {Students[5],Students[6],Students[7],Students[8],Students[9]};
         for (int i = 0; i <Students.length ; i++) {
             Person newStudent = new Student(Students[i][0],Students[i][1],Students[i][2]);
             System.out.println(newStudent.getFullPerson());
@@ -28,6 +30,9 @@ public class application {
             Person newTeacher = new Teacher(Teachers[i][0],Teachers[i][1],Teachers[i][2],Teachers[i][3]);
             System.out.println(newTeacher.getFullPerson()+" Bransi: "+ ((Teacher) newTeacher).getSpecialty());
         }
-
+        classroom class1 = new classroom(Teachers[0], group1,"Schübelbach","A");
+        classroom class2 = new classroom(Teachers[1], group2,"Schwyz","B");
+        System.out.println(class1.getClasses());
+        System.out.println(class2.getClasses());
     }
 }
--- src/week1/assignment1/classroom.java
+++ src/week1/assignment1/classroom.java
@@ -1,7 +1,20 @@
 package week1.assignment1;
 
 public class classroom {
-/* Diger kisimlarda birseyler yapmaya calistim ama
-    Burda nasil yada ne yapacagimi tam olarak anliyamadim.
-*/
+        private String[] teacher;
+        private String[][] students;
+        private String location;
+        private String name;
+
+
+    public classroom(String[] teacher, String[][] group1, String Ort, String zimmer) {
+        this.teacher= teacher;
+        this.students=group1;
+        this.location=Ort;
+        this.name=zimmer;
+    }
+    public String getClasses (){
+        return this.teacher +" "+this.students+" "+" location: "+this.location+" Sinif: "+this.name;
+    }
+
 }
